7e6a939218459a91a87a11f71029c38c74904bcc
THONWELLING <thonwellingdani13@gmail.com>
2022-04-26 23:46:09 -0300

    iniciando o servidor

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response, ErrorRequestHandler } from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+
+dotenv.config()
+
+const server = express()
+
+server.use(cors())
+
+server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.urlencoded({ extended: true }))
+
+server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }))
+
+
+
+server.listen(process.env.PORT)
