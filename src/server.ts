ccd8a3334496fdd936c174a6ae39900b19b4bf4f
THONWELLING <thonwellingdani13@gmail.com>
2022-04-27 00:17:09 -0300

    Criando um endpoint para lidar com erros

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express, { Request, Response, ErrorRequestHandler } from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import apiRoutes from'./routes/api'
 
 
 dotenv.config()
 
+//instanciando o servidor 
 const server = express()
 
+// Para corrigir erros de cors
 server.use(cors())
 
 server.use(express.static(path.join(__dirname, '../public')))
@@ -15,6 +18,23 @@ server.use(express.urlencoded({ extended: true }))
 
 server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }))
 
+//usando as rotas do nosso arquivo routes
+server.use(apiRoutes)
+
+//Criando uma rota  de página não encontrada
+server.use((req: Request, res: Response) => {
+  res.status(404)
+  res.json({ error: 'Endpoint Not Found!!' })
+})
+
+
+//Criando um endpoint para lidar com erros
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(400)  // Bad Request, falta de informação ou requisição fora da regra
+  console.log(err)
+  res.json({ error: 'Something Wrong Happened!!' })
+}
+server.use(errorHandler)
 
 
 server.listen(process.env.PORT)
