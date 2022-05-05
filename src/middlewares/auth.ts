54a332423c6a254febaeaad8775e550c9fc60eb9
THONWELLING <thonwellingdani13@gmail.com>
2022-05-04 23:56:07 -0300

    validando o token gerado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,11 @@
 import { Request, Response, NextFunction } from "express"
+import JWT from'jsonwebtoken'
+import dotenv from'dotenv'
 
+
+
+
+dotenv.config()
 
 export const Auth = {
   private: (req: Request, res: Response, next: NextFunction) => {
@@ -7,7 +13,18 @@ export const Auth = {
     let success = false
 
     // Fazer verificação de auth
+    if(req.headers.authorization) {
+      const [authType, token] = req.headers.authorization.split( ' ' )
+      if(authType === 'Bearer') {
+        try {
+          JWT.verify(token, process.env.JWT_SECRET as string)
+          success = true
+        } catch(err) {
+          
+        } 
+      }
 
+    }
     if(success) {
       next()
     } else {
